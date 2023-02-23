1bdc6df3c2cf265fa5a29ff972a5c75d23ff05be
MariemBaccari <mariembaccari70@gmail.com>
2023-02-23 22:57:51 +0100

    initial commit

---
+++ app/src/androidTest/java/com/github/MariemBaccari/mediato/GreetingActivityTest.java
@@ -0,0 +1,58 @@
+package com.github.MariemBaccari.mediato;
+
+import static android.system.Os.close;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import android.content.Context;
+import android.content.Intent;
+import android.widget.TextView;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class GreetingActivityTest {
+
+    @Test
+    public void useAppContext() {
+        Intents.init();
+        Intent myIntent = new Intent(ApplicationProvider.getApplicationContext(), GreetingActivity.class);
+        myIntent.putExtra("username", "Mariem");
+        ActivityScenario test = ActivityScenario.launch(myIntent);
+        onView(withId(R.id.greetingMessage)).check(
+          matches(
+                  withText("Greetings Mariem !")
+          )
+        );
+        Intents.release();
+    }
+}
